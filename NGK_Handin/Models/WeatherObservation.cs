9d8e9b6019930f3d809481a9272d8140cac3ddf4
MortenBE <Ehlersmorten@gmail.com>
2020-05-19 20:26:28 +0200

    Added WeatherObservation model

---
+++ NGK_Handin/Models/WeatherObservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGK_Handin3.Models
+{
+    public class WeatherObservation
+    {
+        public string LocationName { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }        
+        public double Temperature { get; set; }
+        public int Humidity { get; set; }
+        public double AtmosphericPressure { get; set; }
+    }
+}
