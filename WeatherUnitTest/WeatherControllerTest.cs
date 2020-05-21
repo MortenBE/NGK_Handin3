fd69307f93302a951df984bdc0ddff9f5e4a976c
Jonathan Hartvigsen Juncker <au618464@uni.au.dk>
2020-05-21 23:14:22 +0200

    Test

---
+++ WeatherUnitTest/WeatherControllerTest.cs
@@ -0,0 +1,107 @@
+using NUnit.Framework;
+using NGK_Handin3.Controllers;
+using NGK_Handin3.Models;
+using NGK_Handin3;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NGK_Handin3.Hubs;
+
+namespace WeatherUnitTest
+{
+    public class WeatherControllerTest
+    {
+        private WeatherObservationController _uut;
+        private DbContextOptions<ApplicationDbContext> _options;
+        private ApplicationDbContext _context;
+
+        [SetUp]
+        public void setup() 
+        {
+
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test").Options;
+            _context = new ApplicationDbContext(_options);
+            _uut = new WeatherObservationController(_context);
+        }
+
+        [Test]
+        public async Task get_three_weather_observations() 
+        {
+            WeatherObservation weatherObservation1 = new WeatherObservation
+            {
+                Id = 1,
+                Day = 24,
+                Month = 3,
+                Year = 2020,
+                LocationName = "Holsterbro",
+                Lat = 22.77,
+                Lon = 44.77,
+                Temperature = 23,
+                Humidity = 5,
+                AtmosphericPressure = 56
+            };
+
+            
+
+            WeatherObservation weatherObservation2 = new WeatherObservation
+            {
+                Id = 2,
+                Day = 16,
+                Month = 2,
+                Year = 2020,
+                LocationName = "Skanderborg",
+                Lat = 80.30,
+                Lon = 90.11,
+                Temperature = 23,
+                Humidity = 5,
+                AtmosphericPressure = 56
+            };
+
+            
+
+            WeatherObservation weatherObservation3 = new WeatherObservation
+            {
+                Id = 3,
+                Day = 23,
+                Month = 1,
+                Year = 2020,
+                LocationName = "Randers",
+                Lat = 40.22,
+                Lon = 88.33,
+                Temperature = 16,
+                Humidity = 5,
+                AtmosphericPressure = 56
+            };
+            
+
+
+            WeatherObservation weatherObservation4 = new WeatherObservation
+            {
+                Id = 4,
+                Day = 14,
+                Month = 2,
+                Year = 2020,
+                LocationName = "Randers",
+                Lat = 22.55,
+                Lon = 33.76,
+                Temperature = 10,
+                Humidity = 1,
+                AtmosphericPressure = 20
+            };
+            _context.Add(weatherObservation1);
+            _context.Add(weatherObservation2);
+            _context.Add(weatherObservation3);
+            _context.Add(weatherObservation4);
+            _context.SaveChanges();
+
+            var result = (await _uut.GetWeatherObservations()) as ObjectResult;
+
+            Assert.Equals(3, result.Count);
+        }
+    }
+}
