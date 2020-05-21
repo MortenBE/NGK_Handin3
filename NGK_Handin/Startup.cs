8b7191c3628495e29310a6fc957fa57b7dde2d41
Tobias Holm <201608355@post.au.dk>
2020-05-22 00:01:14 +0200

    lkmafsadflk af æladf ljnasjND JNLADFSLÆ

--- NGK_Handin/Startup.cs
+++ NGK_Handin/Startup.cs
@@ -52,6 +52,8 @@ namespace NGK_Handin3
                     ValidateAudience = false
                 };
             });
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,6 +75,10 @@ namespace NGK_Handin3
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
                 endpoints.MapHub<UpdateHub>("/UpdateHub");
             });
         }
